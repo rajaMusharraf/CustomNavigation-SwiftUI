60274bc9ae1eec897c6498a36bbe46a41f1e2f88
Raja Musharraf <musharraf.ajaz@pral.com.pk>
2024-09-24 09:56:05 +0500

    Initial Commit

---
+++ CustomNavigationBar/CustomNavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationBarApp.swift
+//  CustomNavigationBar
+//
+//  Created by Musharaf on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
